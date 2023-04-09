f6545bd8cc3971ce88140fa7c8aab0bd1910512d
roni <60136750+Roni769361@users.noreply.github.com>
2023-04-09 17:11:45 +0600

    final2

--- custom.js/apps.js
+++ custom.js/apps.js
@@ -1,18 +1,1 @@
 new WOW().init();
-
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  var button = document.getElementById("scroll-to-top-btn");
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    button.style.display = "block";
-  } else {
-    button.style.display = "none";
-  }
-}
-
-document.getElementById("scroll-to-top-btn").addEventListener("click", function() {
-  document.body.scrollTop = 0; /* For Safari */
-  document.documentElement.scrollTop = 0; /* For Chrome, Firefox, IE and Opera */
-});
